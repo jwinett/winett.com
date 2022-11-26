d1082514b0bcadd31b60d093155f86427a59226b
Joe Winett <joe@winett.com>
2022-11-26 16:03:25 -0600

    Hooked up a new page

--- posts/2022/11/this-is-a-c-file.html.c
+++ posts/2022/11/this-is-a-c-file.html.c
@@ -1,9 +1,11 @@
 __BEGIN_PAGE( This is a c file )
 
-
 <h1>This is a c file!</h1>
+<p><a href = "/">winett.com/</a>&nbsp; &nbsp;<a href="/2022/">2022/</a>&nbsp; &nbsp; <a href="/2022/11/">11/</a>&nbsp; &nbsp; <a href = "/2022/11/this-is-a-c-file/">this-is-a-c-file/</a></p>
+
 <p>I mean, I used the c preprocessor to replace some macros to create this .html file.</p>
 
+<p>On Github: <a href="https://github.com/jwinett/winett.com/blob/main/build/cpp.sh">/main/build/cpp.sh</a></p>
 
 
 __END_PAGE(2022-11-26 15:51 CST)
